69ddd10616cb86d23ec7bd932dfb1497f224c04e
Josh60169 <yashvihangupta@gmail.com>
2024-06-23 19:07:02 -0500

    setting up react with vite

---
+++ random-quote-machine/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p>Hello</p>
+    );
+  }
+}
